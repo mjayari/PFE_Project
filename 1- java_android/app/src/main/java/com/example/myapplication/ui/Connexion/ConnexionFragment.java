f16052be9f837cb8f962e184aa45b5ad481968a7
SalimAttia <salimattia18@gmail.com>
2021-07-20 21:47:04 +0100

    Adding the source code for the Main Interface

---
+++ 1- java_android/app/src/main/java/com/example/myapplication/ui/Connexion/ConnexionFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui.Connexion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class ConnexionFragment extends Fragment {
+
+
+    private  Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_connexion,container,false);
+    }
+
+}
