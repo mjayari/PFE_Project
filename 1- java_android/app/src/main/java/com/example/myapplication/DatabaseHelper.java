817d474e4bf01cb0dccbe886b30971e9b9eabbac
SalimAttia <salimattia18@gmail.com>
2021-08-10 14:19:52 +0100

    -Create the DatabaseHelper

--- 1- java_android/app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ 1- java_android/app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,17 +8,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "User";
+    private static final String TABLE_USER = "user";
+    private static final String KEY_ID = "user-id";
+    private static final String KEY_PASSWORD = "Password";
+    private static final String KEY_SIGNUP = "Signup_date";
+
+
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_USER + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_PASSWORD + " TEXT,"
+                + KEY_SIGNUP + " TEXT" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        onCreate(db);
 
     }
-}
+    void addUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_PASSWORD, user.getpassword());
+        values.put(KEY_SIGNUP, user.getsignup());
+
+
+
+    }
