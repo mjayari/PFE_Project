5c296cafc5d95090bcee77f9dd47b9294270dc43
SalimAttia <salimattia18@gmail.com>
2021-08-30 16:51:35 +0100

    -Adding Validation Regex for UserID and Password

--- 1- java_android/app/src/main/java/com/fst/myapplication/ui/Connexion/ConnexionFragment.java
+++ 1- java_android/app/src/main/java/com/fst/myapplication/ui/Connexion/ConnexionFragment.java
@@ -2,6 +2,8 @@ package com.fst.myapplication.ui.Connexion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,9 @@ import com.fst.myapplication.db.DatabaseHelper;
 import com.fst.myapplication.db.User;
 import com.fst.myapplication.ui.Filetransfer.FiletransferFragment;
 import com.fst.myapplication.ui.home.HomeFragment;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConnexionFragment extends Fragment {
 
@@ -68,15 +73,27 @@ public class ConnexionFragment extends Fragment {
                 String userid = binding.useridInput.getText().toString();
                 String password = binding.passwordInput.getText().toString();
 
-                // Toast.makeText(ConnexionFragment.super.getContext(), "Button pressed", Toast.LENGTH_SHORT).show();
 
-                // UserID input verification
-                if (userid.equals(""))
-                    Toast.makeText(ConnexionFragment.super.getContext(), "Please enter UserID field", Toast.LENGTH_SHORT).show();
+
+                // Toast.makeText(ConnexionFragment.super.getContext(), "Button pressed", Toast.LENGTH_SHORT).show();// UserID input verification
+                // if (userid.equals(""))
+                // Toast.makeText(ConnexionFragment.super.getContext(), "Please enter UserID field", Toast.LENGTH_SHORT).show();
 
                 // Password input verification
-                if (password.equals(""))
-                    Toast.makeText(ConnexionFragment.super.getContext(), "Please enter Password field", Toast.LENGTH_SHORT).show();
+                //if (password.equals(""))
+                //    Toast.makeText(ConnexionFragment.super.getContext(), "Please enter Password field", Toast.LENGTH_SHORT).show();
+
+                // Function for validate UserID
+                //binding.passwordInput.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                boolean check= validateinfo(userid,password);
+                Log.d("password", password);
+                //boolean check=true;
+                //if(check==true) {
+                //  Toast.makeText(ConnexionFragment.super.getContext(), "Valid ", Toast.LENGTH_SHORT).show();
+                //}
+                //else {
+                //  Toast.makeText(ConnexionFragment.super.getContext(), "Invalid ", Toast.LENGTH_SHORT).show();
+                //}
 
                 // request Verification of exsitence of USerID and password in DB
                 Boolean checkuserpass = db.checkusernamepassword(userid, password);
@@ -92,15 +109,7 @@ public class ConnexionFragment extends Fragment {
                 //Toast.makeText(ConnexionFragment.super.getContext(), "UserID = " + userid , Toast.LENGTH_SHORT).show();
                 //Toast.makeText(ConnexionFragment.super.getContext(), "ConnexionRowsNumber = " + db.getConnexionRowsNumber() , Toast.LENGTH_SHORT).show();
 
-                // Function for validate UserID
-                boolean check= validateinfo(userid,password);
-                 //boolean check=true;
-                if(check==true) {
-                    Toast.makeText(ConnexionFragment.super.getContext(), "Valid ", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(ConnexionFragment.super.getContext(), "Invalid ", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
 
@@ -129,20 +138,70 @@ public class ConnexionFragment extends Fragment {
         });
     }
 
-    private boolean validateinfo(String userid,String password) {
-       if (userid.length()==0){
+    private boolean validateinfo(String userId,String password) {
+        String userIdREGEX = "^" +
+                "(?=.+[0-9])" +         //at least 1 digit
+                "(?=.*[a-z])" +         //at least 1 lower case letter
+                "(?=.*[a-z0-9])" +      //only lower case letters and numbers
+                "(?=\\S+$)" +           //no white spaces
+                ".{8,15}" +               //size between 8 and 15 characters
+                "$";
+        Pattern p = Pattern.compile(userIdREGEX);
+        Matcher m = p.matcher(userId);
+        boolean b = m.matches();
+        if (b)
+            Toast.makeText(ConnexionFragment.super.getContext(), "UserId Valid", Toast.LENGTH_SHORT).show();
+        else {
+            binding.useridInput.requestFocus();
+            binding.useridInput.setError("UserId not Valid");
+            Toast.makeText(ConnexionFragment.super.getContext(), "UserId not  Valid", Toast.LENGTH_SHORT).show();
+        }
+
+
+        String  passwordREGEX = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                "(?=.*[a-z])" +         //at least 1 lower case letter
+                "(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{8,15}" +               //size between 8 and 15 characters
+                "$";
+        Pattern p1 = Pattern.compile(passwordREGEX);
+        Matcher m1 = p1.matcher(password);
+        boolean b1 = m1.matches();
+        if (b1)
+            Toast.makeText(ConnexionFragment.super.getContext(), "Password Valid", Toast.LENGTH_SHORT).show();
+        else{
+            binding.passwordInput.requestFocus();
+            binding.passwordInput.setError("Password not Valid");
+            binding.passwordInput.setCursorVisible(true);
+            Toast.makeText(ConnexionFragment.super.getContext(), "Password not  Valid", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
+
+      /* if (userid.length()==0){
            binding.useridInput.requestFocus();
            binding.useridInput.setError("Field can't be Empty");
            return false;
        }
-       else if (userid.length()<10){
+       else if (!userid.matches("^.{8,15}$")){
            binding.useridInput.requestFocus();
-           binding.useridInput.setError("At least 10 characters");
+           binding.useridInput.setError("length must be between 8 and 15");
             return false;
        }
-       else if (userid.matches("[A-Za-z0-9]+")){
+       else if (! userid.matches("[0-9]+")){
            binding.useridInput.requestFocus();
-           binding.useridInput.setError("only letters and numbers");
+           binding.useridInput.setError("At least one digit");
+           return false;
+       }
+       else if (! userid.matches("[a-z]+")){
+           binding.useridInput.requestFocus();
+           binding.useridInput.setError("At least one lowercase letter");
            return false;
        }
        else if (userid.matches("[^a!c][@#$%^&+=]+")){
@@ -176,7 +235,7 @@ public class ConnexionFragment extends Fragment {
             binding.passwordInput.setError("Minimum one Symbol");
             return false;
         }
-        else
+        else*/
          return true;
 
     }
