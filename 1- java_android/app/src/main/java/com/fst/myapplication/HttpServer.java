dfe029c4e5c5c5da8664157f9bdd1595a7880439
SalimAttia <salimattia18@gmail.com>
2021-09-05 18:22:30 +0100

    -Creation of HttpServer Class

---
+++ 1- java_android/app/src/main/java/com/fst/myapplication/HttpServer.java
@@ -0,0 +1,19 @@
+package com.fst.myapplication;
+import com.fst.myapplication.db.Configuration;
+
+import java.net.ServerSocket;
+
+public class HttpServer {
+
+    public  static void main(String[] args){
+        System.out.println("server Starting...");
+
+        //Configuration.getInstance().loadConfiguration("/storage/file");
+
+        //System.out.println("Using port: " + conf.getPort());
+        //System.out.println("Using webroot " + conf.getwebroot());
+
+        ServerSocket serverSocket = new ServerSocket()
+
+    }
+}
